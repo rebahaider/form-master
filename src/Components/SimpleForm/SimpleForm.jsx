8bd9f2128e090366d5e24709b1cf0472c1c3acf1
rebahaider <rebahaiderema@gmail.com>
2024-03-23 13:36:38 +0600

    first commit

---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,12 @@
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name="" id="" /><br />
+                <input type="submit" name="" id="" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
